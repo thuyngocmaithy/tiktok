511609bc059b8cf5d4d8c1dcefd636c702684537
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2023-08-26 13:24:33 +0700

    Bài 67: Xây dựng UI phần Header #4

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
@@ -9,8 +17,25 @@ import styles from './Header.module.scss';
 import images from '../../../../assets/images';
 import { Wrapper as PopperWrapper } from '../../../Popper';
 import AccountItem from '../../../AccountItem';
+import Menu from '../../../Popper/Menu';
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -25,8 +50,9 @@ function Header() {
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok"></img>
                 <Tippy
-                    interactive
-                    visible={searchResult.length > 0}
+                    interactive //tippy được tương tác mà không ẩn đi
+                    visible={searchResult.length > 0} //Visible là Hiển thị không cần hover
+                    // Hiển thị khi kết quả tìm kiếm có length > 0
                     render={(attrs) => (
                         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                             <PopperWrapper>
@@ -56,6 +82,12 @@ function Header() {
                     {/* <Button primary href="https://www.google.com.vn/" target="_blank">Log in</Button> */}
                     {/* target="_blank" là mở href ở tab mới */}
                     {/* prop target hoạt động khi truyền thêm ...passProps bên Button/index.js */}
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
