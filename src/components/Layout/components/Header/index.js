af2828326e558bffbae1be72c93b4bdf3744ce75
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2023-08-27 14:44:45 +0700

    Bài 69: Xây dựng UI phần Header #6

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -7,10 +7,17 @@ import {
     faEarthAsia,
     faCircleQuestion,
     faKeyboard,
+    faCloudUpload,
+    faUser,
+    faCoins,
+    faGear,
+    faArrowRightFromBracket,
 } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
-import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+import HeadlessTippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 
 import Button from '../../../Button';
 import styles from './Header.module.scss';
@@ -26,7 +33,7 @@ const MENU_ITEMS = [
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
         children: {
-            title:'Language',
+            title: 'Language',
             data: [
                 {
                     code: 'en',
@@ -35,9 +42,9 @@ const MENU_ITEMS = [
                 {
                     code: 'vi',
                     title: 'Tiếng Việt',
-                }
-            ]
-        }
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -53,6 +60,8 @@ const MENU_ITEMS = [
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
 
+    const currentUser = true;
+
     useEffect(() => {
         setTimeout(() => {
             setSearchResult([]); //[] rỗng để ẩn đi popper tìm kiếm
@@ -61,14 +70,38 @@ function Header() {
 
     const handleMenuChange = (menuItem) => {
         console.log(menuItem);
+    };
 
-    }
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/nva',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coin',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/Settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faArrowRightFromBracket} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
 
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok"></img>
-                <Tippy
+                <HeadlessTippy
                     interactive //tippy được tương tác mà không ẩn đi
                     visible={searchResult.length > 0} //Visible là Hiển thị không cần hover
                     // Hiển thị khi kết quả tìm kiếm có length > 0
@@ -94,18 +127,37 @@ function Header() {
                             <FontAwesomeIcon icon={faMagnifyingGlass} />
                         </button>
                     </div>
-                </Tippy>
+                </HeadlessTippy>
                 <div className={cx('actions')}>
-                    <Button text>Upload</Button>
-                    <Button primary>Log in</Button>
-                    {/* <Button primary href="https://www.google.com.vn/" target="_blank">Log in</Button> */}
-                    {/* target="_blank" là mở href ở tab mới */}
-                    {/* prop target hoạt động khi truyền thêm ...passProps bên Button/index.js */}
-
-                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
-                        <button className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </button>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                            {/* <Button primary href="https://www.google.com.vn/" target="_blank">Log in</Button> */}
+                            {/* target="_blank" là mở href ở tab mới */}
+                            {/* prop target hoạt động khi truyền thêm ...passProps bên Button/index.js */}
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <img
+                                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/4be145ab73bac1279a5588d11cebb885~c5_300x300.webp?x-expires=1693119600&x-signature=%2FcqzhR4axw7sXX3O4OYKCkAHUAA%3D"
+                                className={cx('user-avatar')}
+                                alt="Nguyen Van A"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
                     </Menu>
                 </div>
             </div>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -9,28 +9,30 @@ import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
-const defaultFn = () => {}
+const defaultFn = () => {};
 
 function Menu({ children, items = [], onChange = defaultFn }) {
-
-    const [history, setHistory] = useState([{data: items}])
-    const current = history[history.length-1] //Phần tử cuối (children)
-
-
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1]; //Phần tử cuối (children)
 
     const renderItems = () => {
-        return current.data.map((item, index) => {             
-            const isParent = !!item.children //!! để convert về boolean
-            return <MenuItem key={index} data={item} onClick={() => {
-                if(isParent) //Nếu có children
-                {
-                    // Thêm item.children
-                    setHistory(prev => [...prev, item.children])
-                }
-                else {
-                    onChange(item)
-                }
-            }} /> 
+        return current.data.map((item, index) => {
+            const isParent = !!item.children; //!! để convert về boolean
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            //Nếu có children
+                            // Thêm item.children
+                            setHistory((prev) => [...prev, item.children]);
+                        } else {
+                            onChange(item);
+                        }
+                    }}
+                />
+            );
         });
     };
 
@@ -38,18 +40,28 @@ function Menu({ children, items = [], onChange = defaultFn }) {
         <Tippy
             interactive
             delay={[0, 700]} //Khi show không bị delay
-            // Khi ẩn bị delay 500ms
+            // Khi ẩn bị delay 500ms'
+            offset={[12,8]}
+            placement='bottom-end'
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
                     <PopperWrapper className={cx('menu-popper')}>
-                        {history.length>1 && <Header title="Language" onBack={() => {
-                            //Xóa phần tử cuối (children)
-                            setHistory(prev => prev.slice(0,prev.length-1))
-                        }}/>}
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    //Xóa phần tử cuối (children)
+                                    setHistory((prev) => prev.slice(0, prev.length - 1));
+                                }}
+                            />
+                        )}
                         {renderItems()}
                     </PopperWrapper>
                 </div>
             )}
+            onHide={() => {
+                setHistory((prev) => prev.slice(0, 1));
+            }}
         >
             {children}
         </Tippy>
